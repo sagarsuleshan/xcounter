c705ecec16d85d7f5f70956e43eb58fd860695c2
sagarsuleshan <sagarsuleshan@gmail.com>
2025-07-07 14:47:13 +0530

    Final Commit for XCounter

---
+++ src/CounterApp.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default CounterApp;
